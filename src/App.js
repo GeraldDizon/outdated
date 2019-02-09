c17b711e1bc3d973b304c84568b144f978fa4870
naruga1 <gdizon@xtendly.com>
2019-02-09 16:22:34 +0800

    gerald

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Grid>
+
+          <Row>
+          <div className="sidenav">
+            <Col xs={12} sm={12} md={12} lg={false}>
+                <div className ="nav-logo">
+                  <p>LOGO</p>
+                  <i class="material-icons">
+account_balance</i> 
+                </div>
+                <div className = "nav-control">
+                  <p>HOME</p>
+                  <p>PROJECTS</p>
+                  <p>CONTACT ME</p>
+                </div>
+                <div class = "nav-network">
+                  <p>FACEBOOK</p>
+                </div>
+
+          </Col>
+          </div>
+          </Row>
+
+        </Grid> 
+        <Grid fluid>
+          <Row>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <div className = "bg-home-content">
+                <div className="home-content">
+
+                  <h2>Hello There!</h2>
+                  <h2>I'm Gerald</h2>
+                  <h2>Designer and Developer.</h2>
+                  <hr align="left"/>
+                  <p>FRONT END DEVELOPER</p>
+                  <p>HTML / CSS / JAVASCRIPT / PHP</p> 
+                  <p>CONTACT ME</p>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <h2>Hello There!</h2>
+              <i class="material-icons">face</i>
+            </Col>
+          </Row>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
