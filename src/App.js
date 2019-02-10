ee8091746c69da6ad193b7a0add3f05ca3a9ba96
naruga1 <gdizon@xtendly.com>
2019-02-10 11:54:23 +0800

    Add Image

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ class App extends Component {
             <Col xs={12} sm={12} md={6} lg={6}>
               <div className = "bg-home-content">
                 <div className="home-content">
-
                   <h2>Hello There!</h2>
                   <h2>Im Gerald</h2>
                   <h2>Designer and Developer.</h2>
@@ -43,7 +42,10 @@ class App extends Component {
               </div>
             </Col>
             <Col xs={12} sm={12} md={6} lg={6}>
-              <h2>Hello There!</h2>
+            <div className ='bg-home-picture'>
+              <h2></h2>
+            </div>
+
             </Col>
           </Row>
         </Grid>
