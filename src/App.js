5001fc4a7eca8804d81c52c14260700c63d8c2d6
naruga1 <gdizon@xtendly.com>
2019-02-11 21:20:40 +0800

    Enhance-React-Router

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import HomeIcon from '@material-ui/icons/HomeOutlined';
-import CollectionsIcon from '@material-ui/icons/CollectionsOutlined';
-import PhoneIphoneIcon from '@material-ui/icons/PhoneIphoneOutlined';
-import facebookLogo from './Images/facebookLogo.png';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Collection from './components/Collection';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Grid>
-          <Row>
-            <div className="sidenav">
-              <Col xs={12} sm={12} md={12} lg={false}>
-                  <div className ="nav-logo">
-                    <p>LOGO</p>
-                  </div>
-                  <div className = "nav-control">
-                    <li><HomeIcon style={{color:"white", fontSize:"25px"}}/></li>
-                    <li><CollectionsIcon style={{color:"white", fontSize:"25px"}}/></li>  
-                    <li><PhoneIphoneIcon style={{color:"white", fontSize:"25px"}}/></li>  
-                  </div>
-                  <div class = "nav-network">
-                  <img src={facebookLogo} alt="facebook" style={{height:"30px"}}/>  
-                  </div>
-              </Col>
-            </div>
-          </Row>
-        </Grid> 
-        <Grid fluid>
-          <Row>
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <div className = "bg-home-content">
-                <div className="home-content">
-                
-                  <h2>Hello There!</h2>
-                  <h2>Im Gerald</h2>
-                  <h2>Designer and Developer.</h2>
-                  <hr align="left"/>
-                  <p>FRONT END DEVELOPER</p>
-                  <p>HTML / CSS / JAVASCRIPT / PHP</p> 
-                  <p>CONTACT ME</p>
-                </div>
-              </div>
-            </Col>
-            <Col xs={12} sm={12} md={6} lg={6}>
-            <div className ='bg-home-picture'>
-              <p></p>
-            </div>
-
-            </Col>
-          </Row>
-        </Grid>
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          <Route path='/Collection' component={Collection}/>
+        </React.Fragment> 
+      </BrowserRouter>
     );
   }
 }
