6309d90d27c5348a8759e1bf20c0d4a7345db097
naruga1 <gdizon@xtendly.com>
2019-03-08 16:55:11 +0800

    latest

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import About from './components/About';
-import { BrowserRouter as Router, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 class App extends Component {
+  
   render() {
     return ( 
       <Router>
         <React.Fragment>
           <Navbar />
-          <Route path='/Home' exact strict component={Home} />
-          <Route path='/Projects' exact  component={Projects}/>
-          <Route path='/Contact' exact  component={Contact}/>
-          <Route path='/About' exact  component={About}/>
+          <Switch>
+            <Route path='/Home' exact  component={Home}/>
+            <Route path='/Projects' exact  component={Projects}/>
+            <Route path='/Contact' exact  component={Contact}/>
+            <Route path='/About' exact  component={About}/>
+          </Switch>
           </React.Fragment> 
       </Router>
     );
