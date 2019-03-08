a518d51156df640f9c806bd709b81873bd4d364a
naruga1 <gdizon@xtendly.com>
2019-03-08 12:26:07 +0800

    Updates

---
+++ precache-manifest.1afbe35771e23865ddb701c3a3f59cb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "acdb2c2dd96db6402a7a552289f3a91b",
+    "url": "/Portfolio/static/media/ecommerceFront.acdb2c2d.png"
+  },
+  {
+    "revision": "1938ce69a6627f07de75e3e554ba14be",
+    "url": "/Portfolio/static/media/contactBg.1938ce69.jpg"
+  },
+  {
+    "revision": "bda49e62818ac07dbea212c3e6bd5bcf",
+    "url": "/Portfolio/static/media/aboutBg.bda49e62.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "fa872eea7c454c004d29",
+    "url": "/Portfolio/static/js/main.fa872eea.chunk.js"
+  },
+  {
+    "revision": "71c9e5b37f821afddaf6",
+    "url": "/Portfolio/static/js/1.71c9e5b3.chunk.js"
+  },
+  {
+    "revision": "fa872eea7c454c004d29",
+    "url": "/Portfolio/static/css/main.be5e89d4.chunk.css"
+  },
+  {
+    "revision": "71c9e5b37f821afddaf6",
+    "url": "/Portfolio/static/css/1.432f3db3.chunk.css"
+  },
+  {
+    "revision": "5d967d557d270997f768c2ab85bcc72e",
+    "url": "/Portfolio/index.html"
+  }
+];
